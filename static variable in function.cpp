18cf8ad7f22fb4c9000219f35da0bcb47f11deb2
Mentyy <105610218+Mentyyy@users.noreply.github.com>
2022-11-02 18:19:46 +0100

    Update static variable in function.cpp

--- static variable in function.cpp
+++ static variable in function.cpp
@@ -1,3 +1,9 @@
+/*
+Static variables in a Function: 
+When a variable is declared as static, space for it gets allocated for the lifetime of the program. 
+Even if the function is called multiple times, space for the static variable is allocated only once 
+and the value of variable in the previous call gets carried through the next function call.
+*/
 #include <iostream>
 #include <string>
 using namespace std;
@@ -20,3 +26,5 @@ int main()
         demo();
     return 0;
 }
+
+// Output: 0 1 2 3 4
